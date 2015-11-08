fdbccd93c5b38c00af075f02cef8fef3abcfb32f
darthur11 <d-art@list.ru>
2015-11-08 15:13:03 +0600

    Added public folder and db.php file

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+//echo $_POST["note"];
+$sqlite=new SQLite3('wsn_db.sqlite');
+
+/*$sqlite->query("Create Table 'Notes' (
+'note' TEXT(10000) not NULL,
+'date' TIMESTAMP)
+");*/
+//$sqlite->query("Insert into Notes ('note','date') VALUES ('Hello!',datetime('now'))");
+$values=$sqlite->query("Select * from Notes");
+$row=$values->fetchArray(SQLITE3_ASSOC);
+//var_dump($row);
+$sqlite->close();
+
+header("Location: http://127.0.0.2/index.html");
+die();
+?>
